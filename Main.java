9bfad2ce4b2d01be8a1f2f0e85124d4d50ff2d63
Ethanos92 <92122858+Ethanos92@users.noreply.github.com>
2021-10-07 14:27:22 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Heyy que pasaaaa gentee");
+  }
+}
+/*This is a star*/
+//This is another way to comment
+/*Java Variable types
+You have 5 TYPES OF VARIABES IN Java
+Boolean: True/False, only 2 state - boolean
+Integers: Int , always WHOLE number
+Strings: Text , ex "Hello"
+Floats: Decimals, ex 19.99 or -19.99
+Char: Single characters, "a", "B", WRITE A CODE THAT GIVE YOU HOW MANY CHARACTERS ARE IN "pneumonoultramicroscopicsilicovolcanoconiosis"
+
+
+In math x = 4 means 4 = x
+In programming x = 4 (4 is assigned to x)
+
+x = "patty"
+b = "Tomato"
+c = "Cheese"
+d = "Bread"
+e = "Lettuce"
+f = "Ketchup"
+g = "Mayo"
+
+So the template is: type variable = value;
+*/
